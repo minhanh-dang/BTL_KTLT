b3baa5104d0b30a22c881d7b08b4a7dcae0cb705
Dang Minh Anh <minhanh.dang0612@gmail.com>
2021-06-27 01:48:04 +0700

    load project

---
+++ cmake-build-debug/CMakeFiles/QLDiem_Nhom11_125008.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QLDiem_Nhom11_125008.dir/main.cpp.obj"
+  "QLDiem_Nhom11_125008.exe"
+  "QLDiem_Nhom11_125008.exe.manifest"
+  "QLDiem_Nhom11_125008.pdb"
+  "libQLDiem_Nhom11_125008.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QLDiem_Nhom11_125008.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
